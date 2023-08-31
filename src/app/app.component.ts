0c8c328fcd0ead789e16a95ecba0f403aa141d12
caosang92 <caosang92@gmail.com>
2023-08-31 23:22:26 +0900

    C1- Khoi tao

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './home/home.component.html',
+  styleUrls: ['./home/home.component.scss']
+  //'./app.component.html'
+})
+export class AppComponent {
+  title = 'ShopApp-Angular';
+}
